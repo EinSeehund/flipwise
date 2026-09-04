0e536ccede82d6a4c90d2060dba2092e322a7809
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-09-04 11:44:58 +0200

    implement basic error handling in case of invalid request method

--- pages/api/flashcards/index.js
+++ pages/api/flashcards/index.js
@@ -6,6 +6,7 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     const flashcards = await Flashcard.find();
+
     return response.status(200).json(flashcards);
   } else {
     return response.status(405).json({ message: "Method not allowed" });
