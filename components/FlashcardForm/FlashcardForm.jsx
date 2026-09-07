1adb1ff2699aad4d1dd32a24f37374bd5fd929d9
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-09-07 12:01:53 +0200

    give the form some styling

--- components/FlashcardForm/FlashcardForm.jsx
+++ components/FlashcardForm/FlashcardForm.jsx
@@ -1,32 +1,51 @@
 import styled from "styled-components";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
 
 export default function FlashcardForm() {
-  function handleSubmit(event) {
+  const { mutate } = useSWR("/api/flashcards", fetcher);
+
+  async function handleSubmit(event) {
     event.preventDefault();
+
     const formData = new FormData(event.target);
     const formObject = Object.fromEntries(formData.entries());
-    console.log(formObject);
+
+    const response = await fetch("/api/flashcards", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formObject),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+
+    event.target.reset();
   }
 
   return (
     <StyledForm onSubmit={handleSubmit}>
       <h2>Create a new flashcard</h2>
-      <label htmlFor="question">Question</label>
-      <textarea
+      <StyledLabel htmlFor="question">Question</StyledLabel>
+      <StyledTextarea
         name="question"
         id="question"
         placeholder="Enter your question..."
         required
-      ></textarea>
-      <label htmlFor="answer">Answer</label>
-      <textarea
+      ></StyledTextarea>
+      <StyledLabel htmlFor="answer">Answer</StyledLabel>
+      <StyledTextarea
         name="answer"
         id="answer"
         placeholder="Enter the answer..."
         required
-      ></textarea>
-      <label htmlFor="collection">Collection</label>
-      <select name="collection" id="collection" required>
+      ></StyledTextarea>
+      <StyledLabel htmlFor="collection">Collection</StyledLabel>
+      <StyledSelect name="collection" id="collection" required>
         <option value="">-- Please select an option --</option>
         <option value="Art">Art</option>
         <option value="Biology">Biology</option>
@@ -36,8 +55,8 @@ export default function FlashcardForm() {
         <option value="Music">Music</option>
         <option value="Physics">Physics</option>
         <option value="Technology">Technology</option>
-      </select>
-      <button>Create</button>
+      </StyledSelect>
+      <StyledButton>Create</StyledButton>
     </StyledForm>
   );
 }
@@ -48,4 +67,37 @@ const StyledForm = styled.form`
   align-items: center;
   width: 90vw;
   max-width: 400px;
+  margin-bottom: 32px;
+`;
+
+const StyledTextarea = styled.textarea`
+  width: 80%;
+  font-family: inherit;
+  font-size: inherit;
+  padding: 8px;
+  margin-bottom: 16px;
+  border-radius: 8px;
+`;
+
+const StyledLabel = styled.label`
+  margin-bottom: 4px;
+`;
+
+const StyledSelect = styled.select`
+  width: 80%;
+  font-family: inherit;
+  font-size: inherit;
+  padding: 8px;
+  margin-bottom: 32px;
+  border-radius: 8px;
+`;
+
+const StyledButton = styled.button`
+  font-family: inherit;
+  font-size: inherit;
+  padding: 16px 32px;
+  border-radius: 8px;
+  background-color: #5f5fd2;
+  color: white;
+  border: none;
 `;
--- pages/api/flashcards/index.js
+++ pages/api/flashcards/index.js
@@ -10,19 +10,21 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     try {
-      const flashcards = await Flashcard.find();
+      const flashcards = await Flashcard.find().sort({ _id: -1 });
       return response.status(200).json(flashcards);
     } catch (error) {
-      response.status(500).json({ error: "Error retrieving flashcards" });
+      return response
+        .status(500)
+        .json({ error: "Error retrieving flashcards" });
     }
   }
 
   if (request.method === "POST") {
     try {
       await Flashcard.create(request.body);
-      response.status(201).json({ status: "Flashcard created" });
+      return response.status(201).json({ status: "Flashcard created" });
     } catch (error) {
-      response.status(400).json({ error: error.message });
+      return response.status(400).json({ error: error.message });
     }
   }
 
