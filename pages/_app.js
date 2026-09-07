a887545aae447d43eb56cf205a7a34708c3ab2fa
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-09-07 11:16:23 +0200

    lift up the fetch logic to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
 import Header from "@/components/Header/Header";
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
+  const {
+    data: flashcards,
+    isLoading,
+    error,
+  } = useSWR("/api/flashcards", fetcher);
+
   return (
     <>
       <GlobalStyle />
       <Header />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        flashcards={flashcards}
+        isLoading={isLoading}
+        error={error}
+      />
     </>
   );
 }
